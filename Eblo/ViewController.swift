890d9be0f750ef954718e461d3023f4a229069ef
yansong li <857367901@qq.com>
2016-07-31 10:44:26 -0400

    Refactor Pod

--- Eblo/ViewController.swift
+++ Eblo/ViewController.swift
@@ -14,13 +14,15 @@ class ViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    let main = GCDQueue.main
+    main.async { 
+      print("Test")
+    }
   }
 
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
-
-
 }
 
--- Pods/Target Support Files/SiYuanKit/SiYuanKit-umbrella.h
+++ Pods/Target Support Files/SiYuanKit/SiYuanKit-umbrella.h
@@ -1,6 +1,5 @@
 #import <UIKit/UIKit.h>
 
-#import "SiYuan.h"
 
 FOUNDATION_EXPORT double SiYuanKitVersionNumber;
 FOUNDATION_EXPORT const unsigned char SiYuanKitVersionString[];
