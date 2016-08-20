a471ac1a6b689f45c311b6085de2179a80779f39
yansong li <857367901@qq.com>
2016-08-20 10:47:35 -0400

    Add xcconfig files.

--- Eblo/AppManagement/AppManager.swift
+++ Eblo/AppManagement/AppManager.swift
@@ -17,14 +17,17 @@ import SiYuanKit
  - transfer view controller hierarchy.
  */
 public final class AppManager: NSObject {
-  
+
   /// Indicate whether or not the App is suitable to deal with UI related work.
-  /// We use this variable here is because sometimes you need other work to be done before
+  /// We use this variable here because sometimes you need other work to be done before
   /// updating UI.
   var canNavigate: Bool = false
   
+  /// Whether or not we are in the middle of UI related animating.
+  var animating: Bool = false
+
   var rootController: UINavigationController?
-  
+
   // MARK: Public
   public static let sharedInstance = AppManager()
   
@@ -32,10 +35,10 @@ public final class AppManager: NSObject {
   public func allowNavigation() {
     self.canNavigate = true
   }
-  
+
   /// Create the root navigation controller and set it as the rootViewController of
   /// Appdelegate.
-  func createRootNavigationControllerWithAppDelegate(_ appdelegate: UIApplicationDelegate) {
+  public func createRootNavigationControllerWithAppDelegate(_ appdelegate: UIApplicationDelegate) {
     let creatRootNav = {
       self.rootController = self.createRootNavigationController()
       self.rootController?.setViewControllers([EBPortalViewController()], animated: true)
@@ -44,7 +47,16 @@ public final class AppManager: NSObject {
     }
     GCDQueue.runOrAddMainQueue(block: creatRootNav)
   }
-  
+
+  func goToPortalWith(URI: URISource) {
+    guard self.canNavigate else {
+      // TODO(simonli): log system.
+      print("Should call `allowNavigation()` before doing any UI related operation.")
+      return
+    }
+    
+  }
+
   // MARK: Private
   
   func createRootNavigationController() -> UINavigationController {
@@ -52,5 +64,40 @@ public final class AppManager: NSObject {
     return nav
   }
   
+  // MARK: - View Controller Hierarchy
   
+  /// Return the topViewController of current view controller stack.
+  func topController() -> UIViewController? {
+    var topViewController = self.rootController?.topViewController
+    while let topController = topViewController,
+      topNavController = topController.presentedViewController as? UINavigationController {
+      topViewController = topNavController.topViewController
+    }
+    return topViewController
+  }
+
+  // MARK: - UI
+  
+  /// Executing a block when no animation happenning.
+  func waitForAnimationAndExecute(block: () -> ()) {
+    let wrapper = {
+      if (self.animating) {
+        print("Potentially dangerous while animation occuring!")
+        block()
+      }
+    }
+    //
+    if !self.animating {
+      GCDQueue.runOrAddMainQueue(block: wrapper)
+    }
+    GCDQueue.background.async {
+      while self.animating {
+#if DEBUG
+        print("Sleeping")
+#endif
+        Thread.sleep(forTimeInterval: 0.01)
+      }
+      GCDQueue.runOrAddMainQueue(block: wrapper)
+    }
+  }
 }
---
+++ Eblo/AppManagement/EBURIResolver.swift
@@ -0,0 +1,20 @@
+//
+//  EBURIResolver.swift
+//  Eblo
+//
+//  Created by yansong li on 2016-08-19.
+//  Copyright © 2016 YANSONG LI. All rights reserved.
+//
+
+import Foundation
+
+/// The URISource Protocol. Any one adopt to this protocol should provide a URI.
+public protocol URISource {
+  /// URI computed variable.
+  var URI: String { get }
+}
+
+/// URI Resolver.
+public class EBURIResolver {
+  
+}
